13fc4b154d7d5457183ea819b9aa8c286c265666
Michael Birkeland <michael_birkeland@me.com>
2018-09-12 13:10:16 +0200

    static files

--- smfb_vl/settings.py
+++ smfb_vl/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,5 +127,3 @@ LOGOUT_REDIRECT_URL='home'
 LOGIN_REDIRECT_URL = 'home'
 
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
